ba9035cbec6011341771196992bfbeb34a33deaa
jl223vy <jl223vy@outlook.com>
2018-08-08 21:27:18 +0800

    upload version1.0

---
+++ Src/CatchnUpload/CatchnUpload/frm_main.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace CatchnUpload
+{
+    partial class frm_main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_main));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_catch = new System.Windows.Forms.Button();
+            this.btn_upload = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtxb_files = new System.Windows.Forms.RichTextBox();
+            this.txb_path = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txb_id = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "抓取目录：";
+            // 
+            // btn_catch
+            // 
+            this.btn_catch.Location = new System.Drawing.Point(114, 427);
+            this.btn_catch.Name = "btn_catch";
+            this.btn_catch.Size = new System.Drawing.Size(188, 73);
+            this.btn_catch.TabIndex = 1;
+            this.btn_catch.Text = "抓取";
+            this.btn_catch.UseVisualStyleBackColor = true;
+            this.btn_catch.Click += new System.EventHandler(this.btn_catch_Click);
+            // 
+            // btn_upload
+            // 
+            this.btn_upload.Location = new System.Drawing.Point(463, 427);
+            this.btn_upload.Name = "btn_upload";
+            this.btn_upload.Size = new System.Drawing.Size(188, 73);
+            this.btn_upload.TabIndex = 1;
+            this.btn_upload.Text = "上传";
+            this.btn_upload.UseVisualStyleBackColor = true;
+            this.btn_upload.Click += new System.EventHandler(this.btn_upload_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 30);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "抓取文件：";
+            // 
+            // rtxb_files
+            // 
+            this.rtxb_files.Location = new System.Drawing.Point(251, 257);
+            this.rtxb_files.Name = "rtxb_files";
+            this.rtxb_files.Size = new System.Drawing.Size(436, 96);
+            this.rtxb_files.TabIndex = 3;
+            this.rtxb_files.Text = "";
+            // 
+            // txb_path
+            // 
+            this.txb_path.Location = new System.Drawing.Point(251, 164);
+            this.txb_path.Name = "txb_path";
+            this.txb_path.Size = new System.Drawing.Size(436, 42);
+            this.txb_path.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 30);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "用户标识：";
+            // 
+            // txb_id
+            // 
+            this.txb_id.Location = new System.Drawing.Point(251, 73);
+            this.txb_id.Name = "txb_id";
+            this.txb_id.Size = new System.Drawing.Size(436, 42);
+            this.txb_id.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rtxb_files);
+            this.panel1.Controls.Add(this.txb_id);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txb_path);
+            this.panel1.Controls.Add(this.btn_catch);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btn_upload);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(781, 591);
+            this.panel1.TabIndex = 5;
+            // 
+            // frm_main
+            // 
+            this.AcceptButton = this.btn_catch;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(809, 616);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frm_main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "医保异常文件抓取上传";
+            this.Load += new System.EventHandler(this.frm_main_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_catch;
+        private System.Windows.Forms.Button btn_upload;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtxb_files;
+        private System.Windows.Forms.TextBox txb_path;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txb_id;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Src/CatchnUpload/CatchnUpload/frm_main.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.IO;
+using System.Diagnostics;
+
+namespace CatchnUpload
+{
+    public partial class frm_main : Form
+    {
+        private string savePathDir;
+
+        public frm_main()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_catch_Click(object sender, EventArgs e)
+        {
+            bool flag1 = true;
+            string fail = "";
+            string[] files = ConfigurationManager.AppSettings["files"].Split(';');
+            foreach (string f in files)
+            {
+                bool flag2 = CopyFiles(f, ConfigurationManager.AppSettings["sourcePath"], savePathDir);
+                if (!flag2)
+                {
+                    fail += f + ";";
+                    flag1 = false;
+                }
+            }
+            if (flag1)
+            {
+                MessageBox.Show("文件抓取成功！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("部分文件抓取失败！抓取失败文件：\n" + fail, "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private bool CopyFiles(string fileName, string sourcePath, string savePath)
+        {
+            if (!Directory.Exists(savePath))
+            {
+                Directory.CreateDirectory(savePath);
+            }
+
+            try
+            {
+                File.Copy(sourcePath + "\\" + fileName,
+                    savePath + "\\" + DateTime.Now.ToString("yyyyMMddhhmmss") + fileName, true);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void btn_upload_Click(object sender, EventArgs e)
+        {
+            string hostPath = ConfigurationManager.AppSettings["host"] + "\\" + ConfigurationManager.AppSettings["userID"] + "\\";
+
+            //连接共享文件夹
+            bool status = connectState(ConfigurationManager.AppSettings["host"],
+                ConfigurationManager.AppSettings["username"], ConfigurationManager.AppSettings["password"]);
+            if (status)
+            {
+                //MessageBox.Show("服务器连接成功！", "提醒", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                bool hasUploded = uploadFiles(hostPath);
+                if (hasUploded)
+                {
+                    MessageBox.Show("文件上传成功！", "提醒", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("文件上传失败！", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("服务器连接失败！", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private bool connectState(string path, string userName, string passWord)
+        {
+            bool Flag = false;
+            Process proc = new Process();
+            try
+            {
+                proc.StartInfo.FileName = "cmd.exe";
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardInput = true;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.Start();
+                string dosLine = "net use " + path + " " + passWord + " /user:" + userName;
+                proc.StandardInput.WriteLine(dosLine);
+                proc.StandardInput.WriteLine("exit");
+                while (!proc.HasExited)
+                {
+                    proc.WaitForExit(1000);
+                }
+                string errormsg = proc.StandardError.ReadToEnd();
+                proc.StandardError.Close();
+                if (string.IsNullOrEmpty(errormsg))
+                {
+                    Flag = true;
+                }
+                else
+                {
+                    throw new Exception(errormsg);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                proc.Close();
+                proc.Dispose();
+            }
+            return Flag;
+        }
+
+        private bool uploadFiles(string hostPath)
+        {
+            if (!Directory.Exists(hostPath))
+            {
+                Directory.CreateDirectory(hostPath);
+            }
+
+            try
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(savePathDir);
+                foreach (FileInfo f in directoryInfo.GetFiles())
+                {
+                    string filePath = savePathDir + "\\" + f.ToString();
+                    FileStream inFileStream = new FileStream(filePath, FileMode.Open);
+                    FileStream outFileStream = new FileStream(hostPath + f.ToString(), FileMode.OpenOrCreate);
+
+                    byte[] buf = new byte[inFileStream.Length];
+                    int byteCount;
+                    while ((byteCount = inFileStream.Read(buf, 0, buf.Length)) > 0)
+                    {
+                        outFileStream.Write(buf, 0, byteCount);
+                    }
+                    inFileStream.Flush();
+                    inFileStream.Close();
+                    outFileStream.Flush();
+                    outFileStream.Close();
+                    File.Delete(filePath);
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void frm_main_Load(object sender, EventArgs e)
+        {
+            savePathDir = System.IO.Directory.GetCurrentDirectory() + "\\save\\" + DateTime.Now.ToString("yyyyMMdd");
+
+            txb_id.Text = ConfigurationManager.AppSettings["userID"];
+            txb_id.Enabled = false;
+            txb_path.Text = ConfigurationManager.AppSettings["sourcePath"];
+            txb_path.Enabled = false;
+            rtxb_files.Text = ConfigurationManager.AppSettings["files"];
+            rtxb_files.Enabled = false;
+        }
+    }
+}
